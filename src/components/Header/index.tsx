55ca44c62939baad67aaecec16b6cdbe470029a0
saviooribe <saviooribe@gmail.com>
2022-12-22 19:55:18 -0300

    Header & Layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { HeaderContent } from "./styles";
+import logoIgnite from '../../assets/ignite-logo.svg'
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
+
 export function Header() {
-    return <h1 >Enviar</h1>
+    return (
+        <HeaderContent>
+            <img src={logoIgnite} />
+            <nav>
+                <NavLink to="/" title='Timer' >
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to="/history" title="Histórico" >
+                    <Scroll size={24} />
+                </NavLink>
+            </nav>
+        </HeaderContent>
+    )
 } 

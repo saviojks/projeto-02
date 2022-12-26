a4f1132f38f9aee0911993ffb24f2ef6184b3cc0
saviooribe <saviooribe@gmail.com>
2022-12-26 09:56:27 -0300

    Página: History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,72 @@
+import { HistoryContainer, HistoryList } from "./style";
+
 export function History() {
-    return <h1>history</h1>
+    return (
+        <HistoryContainer>
+            <h1>Meu Histórico</h1>
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 Semanas</td>
+                            <td>Concluído</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+    )
 }
